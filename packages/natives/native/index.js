1ef7a9463bd528a27e75512baf75d4d64a7d49da
JAY <jay@JAYui-MacBookPro.local>
2026-07-08 18:03:21 +0700

    chore: bump version to 0.3.8

--- packages/natives/native/index.js
+++ packages/natives/native/index.js
@@ -27,7 +27,7 @@ export const PtySession = nativeBindings.PtySession;
 export const Shell = nativeBindings.Shell;
 
 // functions
-export const __piNativesV0_9_0 = nativeBindings.__piNativesV0_9_0;
+export const __piNativesV0_3_8 = nativeBindings.__piNativesV0_3_8;
 export const applyBashFixups = nativeBindings.applyBashFixups;
 export const astEdit = nativeBindings.astEdit;
 export const astGrep = nativeBindings.astGrep;
