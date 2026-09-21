1f12967dfc42c9dac64ab631b425c6c213fdf5fe
jaybeyond <201892478+jaybeyond@users.noreply.github.com>
2026-09-21 11:29:26 +0700

    chore: bump version to 0.5.24

--- packages/natives/native/index.js
+++ packages/natives/native/index.js
@@ -30,7 +30,7 @@ export const Shell = nativeBindings.Shell;
 
 // functions
 export const __piNativesPublishOutcomeV1 = nativeBindings.__piNativesPublishOutcomeV1;
-export const __piNativesV0_5_23 = nativeBindings.__piNativesV0_5_23;
+export const __piNativesV0_5_24 = nativeBindings.__piNativesV0_5_24;
 export const applyBashFixups = nativeBindings.applyBashFixups;
 export const applyOwnerOnlyFdSecurity = nativeBindings.applyOwnerOnlyFdSecurity;
 export const applyOwnerOnlyPathSecurity = nativeBindings.applyOwnerOnlyPathSecurity;
